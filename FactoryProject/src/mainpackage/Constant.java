9425d415376ed46dbbb4d888d645cd903982fa37
BuglakPavel < >
2014-03-31 13:21:49 +0300

    remove project

--- FactoryProject/src/mainpackage/Constant.java
+++
@@ -1,6 +0,0 @@
-package mainpackage;
-
-public class Constant {
-public static String FireFox ="FireFox";
-public static String Chrome ="Chrome";
-}
--- FactoryProject/src/mainpackage/IChrome.java
+++
@@ -1,7 +0,0 @@
-package mainpackage;
-
-import org.openqa.selenium.WebDriver;
-
-public interface IChrome {
-	public WebDriver CreateCrome();
-}
